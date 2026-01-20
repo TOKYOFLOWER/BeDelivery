451fcf0acc988554ff47da565d1a86a01a3ab7d5
TOKYOFLOWER <tokyoflowercoltd+github@gmail.com>
2026-01-21 02:28:41 +0900

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
  */
 
 // GAS Web AppのURL（デプロイ後に設定）
-const GAS_API_URL = 'YOUR_GAS_API_URL_HERE';
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbzgmm68hvNPJ_dLadw20IOQy3vCQ0qUzolZ-kzyxLazxnyXeFU3Xlq8ShDa2YxnDl1y/exec';
 
 // ★デプロイ後、GASのWeb App URLに変更してください★
 // 例: 'https://script.google.com/macros/s/AKfycbxxxxx.../exec'
